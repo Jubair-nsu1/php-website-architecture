919e79d6e65d85e7ac7ef3b7ac22c4c09809e0b2
Jubair-nsu1 <62503637+Jubair-nsu1@users.noreply.github.com>
2021-12-06 17:15:18 +0600

    update db config

--- controller/dbConnection.php
+++ controller/dbConnection.php
@@ -1,5 +1,5 @@
 <?php
 //	$conn= new mysqli('localhost','root','','bylc')or die("Could not connect to mysql".mysqli_error($con));
 	//Remote Database
-	$conn =mysqli_connect("remotemysql.com", "4yMzXaBzd2","FSD11NaNgZ","4yMzXaBzd2");
+	$conn =mysqli_connect("remotemysql.com", "fMXMG70yS4","1LHpe3GZyy","fMXMG70yS4");
 ?>
